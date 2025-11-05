b0986eb472f71498d0b60428ad9691c994d79696
Mariani <jvtobiasmariani2@gmail.com>
2025-11-05 20:21:39 -0300

    contact list 2

--- hello.c
+++ hello.c
@@ -23,19 +23,47 @@ void addContato(struct Contato contato[], int *total) {
 
     (*total)++;
 }
+
+    void listContatos(struct Contato contato[], int total) {
+        if(total == 0){
+            printf("Lista esta vazia, por favor adicione algum contato\n\n\n");
+            return;
+        }
+
+        for(int i = 0; i < total; i++){
+            printf("%d - %d\n", i, contato[i].nome);
+        }
+
+    }   
 int main() {
     struct Contato contato[100];
+
     int total_contatos;
-    
+    total_contatos = 0;
+    while(1){
+
     int escolha;
-    printf("O que deseja fazer:\n 1 - Adicionar contato\n2 - Listar contatos\n3 - Editar contato\n");
+    printf("O que deseja fazer:\n 1 - Adicionar contato\n2 - Listar contatos\n3 - Pesquisar contato\n0 - Sair\n\n");
     scanf("%d", &escolha);
+    system("cls");
+    //add contato
     if(escolha == 1){
     addContato(contato, &total_contatos);
-
-    total_contatos = 0;
+  
+    
 
     printf("Voce adicionou o contato: %s \n", contato[total_contatos].nome);
-    printf("Sua lista de contato tem %d contatos\n",total_contatos + 1);
+    printf("Sua lista de contato tem %d contatos\n",total_contatos);
+    }
+
+    //sair
+      else if(escolha == 0){
+        break;
+    }
+
+    else if(escolha == 2){
+        listContatos(contato, total_contatos);
+    }
+
     }
 }
